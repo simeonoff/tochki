bf15645d75fc434898dbe6781c5e5da32678214d
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-05-05 15:45:19 +0300

    fix: ssh IdentityAgent string not being quoted

--- modules/home-manager/programs/ssh/default.nix
+++ modules/home-manager/programs/ssh/default.nix
@@ -1,16 +1,15 @@
 { pkgs, config, ... }:
-let
-  onePassPath =
-    if pkgs.stdenv.isDarwin
-    then "/Users/${config.home.username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else "~/.1password/agent.sock";
-in
 {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-          IdentityAgent ${onePassPath}
-    '';
+    extraConfig = 
+      if pkgs.stdenv.isDarwin then  /* bash */ ''
+        Host *
+            # On macOS, we need quotes around the path because it contains spaces
+            IdentityAgent "/Users/${config.home.username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '' else /* bash */ ''
+        Host *
+            IdentityAgent ~/.1password/agent.sock
+      '';
   };
 }
