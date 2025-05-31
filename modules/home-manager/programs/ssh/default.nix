363980f92d028d020f302b73d73f10e9a0aaa5c5
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-05-31 11:48:38 +0300

    feat(ssh): simplify ssh agent config to always use Bitwarden agent
    Remove macOS-specific IdentityAgent path and set a universal default to
    `~/.bitwarden-ssh-agent.sock` for all hosts.

--- modules/home-manager/programs/ssh/default.nix
+++ modules/home-manager/programs/ssh/default.nix
@@ -1,15 +1,10 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   programs.ssh = {
     enable = true;
-    extraConfig = 
-      if pkgs.stdenv.isDarwin then  /* bash */ ''
-        Host *
-            # On macOS, we need quotes around the path because it contains spaces
-            IdentityAgent "/Users/${config.home.username}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-      '' else /* bash */ ''
-        Host *
-            IdentityAgent ~/.bitwarden-ssh-agent.sock
-      '';
+    extraConfig = /* bash */ ''
+      Host *
+          IdentityAgent ~/.bitwarden-ssh-agent.sock
+    '';
   };
 }
