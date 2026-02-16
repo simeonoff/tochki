59608788217cfc22d0988131f07ca10179f5a8b2
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-02-16 10:16:58 +0200

    refactor(home-manager): change default home install location and fix carapace

--- modules/home-manager/programs/carapace/default.nix
+++ modules/home-manager/programs/carapace/default.nix
@@ -1,6 +1,28 @@
-{ ... }: {
+{ pkgs, lib, config, ... }:
+let
+  bin = lib.getExe config.programs.carapace.package;
+in
+{
   programs.carapace = {
     enable = true;
-    enableNushellIntegration = true;
+    # Disable the default nushell integration because the home-manager module
+    # runs `carapace _carapace nushell` at build time inside the nix sandbox
+    # where HOME=/homeless-shelter. On macOS this produces a PATH entry with
+    # "Library/Application Support" (contains a space) which breaks `env`.
+    enableNushellIntegration = false;
   };
+
+  # Provide our own nushell integration with XDG_CONFIG_HOME set at build time
+  # so carapace outputs a space-free path (~/.config/carapace/bin instead of
+  # ~/Library/Application Support/carapace/bin).
+  programs.nushell.extraConfig = ''
+    source ${
+      pkgs.runCommand "carapace-nushell-config.nu" {
+        HOME = "/homeless-shelter";
+        XDG_CONFIG_HOME = "/homeless-shelter/.config";
+      } ''
+        ${bin} _carapace nushell | sed 's|"/homeless-shelter|$"($env.HOME)|g' >> "$out"
+      ''
+    }
+  '';
 }
