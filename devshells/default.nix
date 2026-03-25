f90bc22fbf314a2a4daeff67b52db7cb9980212f
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-03-25 06:07:02 +0200

    fix: pkgs-dotnet not imported

--- devshells/default.nix
+++ devshells/default.nix
@@ -1,9 +1,9 @@
 # Development shells entry point
-{ pkgs, neovim-nightly }:
+{ pkgs, neovim-nightly, pkgs-dotnet }:
 
 let
   nodeShells = import ./node.nix { inherit pkgs; };
-  dotnetShells = import ./dotnet.nix { inherit pkgs; };
+  dotnetShells = import ./dotnet.nix { inherit pkgs pkgs-dotnet; };
   neovimShells = import ./neovim.nix { inherit pkgs neovim-nightly; };
 in
 
